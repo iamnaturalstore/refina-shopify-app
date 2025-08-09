db175e929545c0a18e659cd2e6a1a301a7917a11
Ross Anderson <rossanderson@Mac.modem>
2025-08-09 16:08:01 +1000

    Initial commit for Refina Shopify app

--- _archive-app/routes/_index.jsx
+++
@@ -1,8 +0,0 @@
-export default function Index() {
-  return (
-    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
-      <h1>🎉 Refina is Running</h1>
-      <p>This is your embedded Shopify app homepage.</p>
-    </div>
-  );
-}
