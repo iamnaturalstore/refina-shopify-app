fd21724265e97461660d09e3bf89187cca1a0bdf
Ross Anderson <rossanderson@Mac.modem>
2025-09-14 12:53:19 +1000

    build(admin-ui): final build

--- admin-ui/vite.config.js
+++ admin-ui/vite.config.js
@@ -1,17 +1,13 @@
 // admin-ui/vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   base: '/admin-ui/',
-  resolve: { dedupe: ['react', 'react-dom'] },
-  optimizeDeps: { include: ['react', 'react-dom', '@shopify/polaris'] },
   build: {
-    outDir: '../refina-backend/admin-ui-dist',
+    outDir: path.resolve(__dirname, '../refina-backend/admin-ui-dist'),
     emptyOutDir: true,
-    target: 'es2020',
-    minify: 'esbuild',
-    sourcemap: false,
   },
 });
